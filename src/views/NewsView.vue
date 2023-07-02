506e01d2928ba56eeffdc62b740a98990f24ec22
KimTaeyoun <kimty1121@gmail.com>
2023-07-02 22:06:10 +0900

    Mixin 적용 후 HOC 구조와 비교

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -7,13 +7,12 @@
 <script>
 import ListItem from '../components/ListItem.vue';
 import bus from '../utils/bus.js';
+import ListMixin from '../mixins/ListMixin.js';
 
 export default {
   components: {
     ListItem
   },
-  created() {
-    
-  }
+  mixins: [ListMixin]
 }
 </script>
